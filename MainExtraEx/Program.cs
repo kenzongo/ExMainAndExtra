c532a7ff61b9c80549ec4c51e6e573e1694c9e2e
kenzongo <overdev.khang@gmail.com>
2024-09-17 18:26:06 +0700

    main ex1

--- MainExtraEx/Program.cs
+++ MainExtraEx/Program.cs
@@ -2,7 +2,12 @@
 #region Tính số ngày trong tuần và số ngày lẻ
 Console.WriteLine("Enter number of days:");
 int num = Convert.ToInt32(Console.ReadLine());
-if(num <= 0 || ) {
-
+if (num <= 0)
+{
+    Console.WriteLine("Invalid day!!!");
+    return;
 }
+int weeks = num / 7;
+int remainingdays = num % 7;
+Console.WriteLine($"{num} {(num <= 1 ? "day" : "days")} equals {weeks} {(weeks <= 1 ? "week" : "weeks")} and {remainingdays} {(remainingdays <= 1 ? "day" : "days")} ");
 #endregion
