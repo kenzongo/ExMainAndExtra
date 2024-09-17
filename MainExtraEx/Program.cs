06f0a96d531ea1daa9eab239ec4b55145ab559f0
kenzongo <overdev.khang@gmail.com>
2024-09-17 17:59:43 +0700

    init ex

---
+++ MainExtraEx/Program.cs
@@ -0,0 +1,8 @@
+﻿// Main Excercise
+#region Tính số ngày trong tuần và số ngày lẻ
+Console.WriteLine("Enter number of days:");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num <= 0 || ) {
+
+}
+#endregion
